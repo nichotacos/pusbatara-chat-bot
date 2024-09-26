90bd9931c4d4d220289626d51db9f7313864af3c
Nicholas Yang <nicho.yeo7@gmail.com>
2024-09-26 10:12:21 +0700

    all good until uploading images

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { makeWASocket, useMultiFileAuthState, DisconnectReason, downloadMediaMessage } = require('@whiskeysockets/baileys');
-const cloudinary = require('./utils/cloudinaryConfig');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const cloudinary = require('./utils/cloudinary');
 
 const userStates = {};  // This will store the state of each user and their form responses
 
@@ -15,7 +17,7 @@ const startSock = async () => {
         const { connection, lastDisconnect } = update;
         if (connection === 'close') {
             if (lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut) {
-                startSock(); // Restart if not logged out
+                startSock();
             }
         } else {
             console.log('Connection update:', update);
@@ -28,38 +30,53 @@ const startSock = async () => {
     sock.ev.on('messages.upsert', async (m) => {
         const msg = m.messages[0];
 
-        if (!msg.key.fromMe && msg.key.remoteJid.includes('@s.whatsapp.net') && msg.message) {
+        if (!msg.key.fromMe && msg.key.remoteJid.includes('@s.whatsapp.net') && msg.message && (!msg.key.remoteJid.includes('6288224127612') || !msg.key.remoteJid.includes('6287778500955'))) {
             const userId = msg.key.remoteJid;
             const userInput = msg.message.extendedTextMessage?.text?.trim() || msg.message?.conversation;
-            const userState = userStates[userId] || { currentMenu: 'initial', formData: {} };  // Get user's state, or set to mainMenu
-            const userInputImage = msg.message?.imageMessage; // User input image
+            const userState = userStates[userId] || { currentMenu: 'initial', formData: {}, step: 0 };  // Get user's state, or set to mainMenu
 
             console.log('Message:', JSON.stringify(msg, null, 2));
             console.log('User input:', userInput);
+            console.log('User state:', userState);
+
+            if (userState.step > 0 && userState.currentMenu === 'mainMenu') {
+                userState.currentMenu = 'initial';
+                userState.step = 0;
+            }
+            if (userState.step > 0 && userState.currentMenu != 'mainMenu') {
+                await sendMainMenu(sock, userId);
+                userState.currentMenu = 'mainMenu';
+                userState.step = 0;
+                return;
+            }
 
             switch (userState.currentMenu) {
                 case 'initial':
-                    await handleInitialMenu(sock, userId, userState);
-                    break;
+                    await tesSwitch(sock, userId, userInput, userState);
+                    userStates[userId] = userState;
+                    return;
                 case 'mainMenu':
                     await handleMainMenu(sock, userId, userInput, userState);
                     break;
                 case 'donationMenu':
                     await handleDonationMenu(sock, userId, userInput, userState);
                     break;
-
+                case 'chooseInstallment':
+                    // choose installment type
+                    await handleInstallmentType(sock, userId, userInput, userState);
+                    break;
                 case 'fillForm':
                     await handleFormFilling(sock, userId, userInput, userState);
                     break;
                 case 'sendConfirmation':
                     // give function to handle image input
-                    await handleImageInput(sock, userId, userInputImage, userState);
+                    await handleImageInput(sock, userId, msg, userState);
                     break;
-                default:
-                    await sock.sendMessage(userId, { text: 'Invalid menu state, resetting.' });
-                    userState.currentMenu = 'mainMenu';
-                    await sendMainMenu(sock, userId);
-                    break;
+                // default:
+                //     await sock.sendMessage(userId, { text: 'Invalid menu state, resetting.' });
+                //     userState.currentMenu = 'mainMenu';
+                //     await sendMainMenu(sock, userId);
+                //     break;
             }
 
             userStates[userId] = userState;  // Save the updated state for the user
@@ -69,11 +86,29 @@ const startSock = async () => {
     });
 };
 
+const tesSwitch = async (sock, userId, userInput, userState) => {
+    if (['halo', 'menu', 'tanya', 'p'].includes(userInput.toLowerCase())) {
+        await handleInitialMenu(sock, userId, userState);
+    } else {
+        userState.currentMenu = 'initial'; // Default to 'initial' if no match
+    }
+}
+
 const handleInitialMenu = async (sock, userId, userState) => {
     await sock.sendMessage(userId, {
         text: 'Selamat datang di menu utama. Pilih opsi:\n1. Profil Pusbatara\n2. Update Progress Pusbatara Terkini\n3. Ingin Berdonasi\n4. Konfirmasi Transfer\n5. Cek Outstanding/Riwayat Cicilan\n6. Live Chat Admin'
     });
     userState.currentMenu = 'mainMenu';
+}
+
+const upStep = function (userState) {
+    userState.step++;
+    return userState;
+}
+
+const downStep = function (userState) {
+    userState.step == 0 ? userState.step : userState.step--;
+    return userState;
 }
 
 // Handle main menu logic
@@ -93,8 +128,9 @@ const handleMainMenu = async (sock, userId, input, userState) => {
             break;
         case '3':
             await sock.sendMessage(userId, {
-                text: 'Pilih paket donasi yang anda inginkan:\n\n1. Tumbler Rp.###.###,00\n2. Bor Pile Rp.###.###,00\n3. Kapling Rp.###.###,00\n4. Donasi Bebas'
+                text: 'Pilih paket donasi yang anda inginkan:\n\n1. Tumbler Rp.###.###,00\n2. Bor Pile Rp.###.###,00\n3. Kapling Rp.###.###,00\n4. Donasi Bebas\n\n0. Kembali ke menu utama.'
             });
+            downStep(userState);
             userState.currentMenu = 'donationMenu';  // Move to donation menu
             break;
         case '4':
@@ -116,6 +152,7 @@ const handleMainMenu = async (sock, userId, input, userState) => {
             await sendMainMenu(sock, userId);
             break;
         default:
+            upStep(userState);
             await sock.sendMessage(userId, { text: 'Pilihan tidak tersedia. Harap masukkan pilihan yang tersedia!' });
             await sendMainMenu(sock, userId);
             break;
@@ -143,10 +180,19 @@ const handleDonationMenu = async (sock, userId, input, userState) => {
             selectedPackageName = 'Donasi Bebas';
             selectedPackagePrice = null;
             break;
+        case '0':
+            await sock.sendMessage(userId, {
+                text: 'Kembali ke menu utama.'
+            });
+            await sendMainMenu(sock, userId);
+            userState.currentMenu = 'mainMenu';
+            return;
         default:
+            upStep(userState);
             await sock.sendMessage(userId, {
                 text: 'Paket donasi tidak ditemukan, silahkan pilih paket yang tersedia.'
             });
+            userStates[userId] = userState;
             return;
     }
 
@@ -154,21 +200,76 @@ const handleDonationMenu = async (sock, userId, input, userState) => {
         text: `Anda memilih paket donasi ${selectedPackageName} ${selectedPackagePrice && `seharga ${selectedPackagePrice}`}.`
     });
 
-    // After selecting package, ask user for form input (Name, State, City)
-    userState.currentMenu = 'fillForm';
+    if (input === '4') {
+        await sock.sendMessage(userId, {
+            text: 'Isilah data diri anda menggunakan format berikut. Dapat disalin saja dan isi sesuai kolom yang dibutuhkan. Pastikan data yang anda isi benar:\n\nNama: \nProvinsi: \nKota: '
+        });
+    } else {
+        if (input === '3') {
+            await sock.sendMessage(userId, {
+                text: 'Pilih tipe donasi yang anda inginkan:\n\n1. Sekali Bayar\n2. 10 Kali Cicilan'
+            });
+        } else {
+            await sock.sendMessage(userId, {
+                text: 'Pilih tipe donasi yang anda inginkan:\n\n1. Sekali Bayar\n2. 5 Kali Cicilan'
+            });
+        }
+    }
+
+    // Redirecting to choose installment type
+    userState.currentMenu = 'chooseInstallment';
     userState.formData.package = selectedPackageName;
+};
+
+const handleInstallmentType = async (sock, userId, input, userState) => {
+    let installmentType;
+
+    // /Tumbler|Bor Pile/
+    if (userState.formData.package.includes(['Tumbler', 'Bor Pile'])) {
+        if (input === '1') {
+            installmentType = 'Sekali Bayar';
+            downStep(userState);
+        } else if (input === '2') {
+            installmentType = '5 Kali Cicilan';
+            downStep(userState);
+        } else {
+            upStep(userState);
+            await sock.sendMessage(userId, {
+                text: 'Silahkan pilih cicilan yang tersedia.'
+            });
+            return;
+        }
+    } else {
+        if (input === '1') {
+            installmentType = 'Sekali Bayar';
+            downStep(userState);
+        } else if (input === '2') {
+            installmentType = '10 Kali Cicilan';
+            downStep(userState);
+        } else {
+            upStep(userState);
+            await sock.sendMessage(userId, {
+                text: 'Silahkan pilih cicilan yang tersedia.'
+            });
+            return;
+        }
+    }
 
     await sock.sendMessage(userId, {
-        text: 'Isilah data diri anda menggunakan format berikut. Dapat disalin saja dan isi sesuai kolom yang dibutuhkan:\n\nName: <Your Name>\nState: <Your State>\nCity: <Your City>'
+        text: `Anda memilih tipe donasi ${userState.formData.package} dengan pembayaran ${installmentType}. Terima kasih atas donasi yang telah diberikan.`
     });
-};
+
+    // After selecting package, ask user for form input (Name, State, City)
+    userState.currentMenu = 'fillForm';
+    userState.formData.installmentType = installmentType;
+}
 
 // Handle form filling with one response
 const handleFormFilling = async (sock, userId, input, userState) => {
     const formData = userState.formData;
 
     // Parsing user input to extract Name, State, and City
-    const formRegex = /Name:\s*(?<name>.+)\nState:\s*(?<state>.+)\nCity:\s*(?<city>.+)/i;
+    const formRegex = /Nama:\s*(?<name>.+)\nProvinsi:\s*(?<state>.+)\nKota:\s*(?<city>.+)/i;
     const match = formRegex.exec(input);
 
     if (match && match.groups) {
@@ -188,7 +289,7 @@ const handleFormFilling = async (sock, userId, input, userState) => {
     } else {
         // If the format is incorrect, prompt the user again
         await sock.sendMessage(userId, {
-            text: 'Format data diri tidak tepat. Harap inputkan data diri sesuai dengan format yang diberikan:\n\nName: <Your Name>\nState: <Your State>\nCity: <Your City>'
+            text: 'Format data diri tidak tepat. Harap inputkan data diri sesuai dengan format yang diberikan:\n\nNama: \nProvinsi: \nKota: '
         });
     }
 };
@@ -196,14 +297,34 @@ const handleFormFilling = async (sock, userId, input, userState) => {
 // Handle image input for transfer confirmation
 const handleImageInput = async (sock, userId, input, userState) => {
     // Handle image input here
-    if (input) {
-        const imageBuffer = await downloadMediaMessage(input);
-        const image = await cloudinary.uploader
-            .upload(imageBuffer, {
-                folder: 'pusbatara'
-            });
+    if (input.message.imageMessage) {
+        const imageBuffer = await downloadMediaMessage(
+            input,
+            'buffer',
+            {},
+            { logger: console }
+        );
+        console.log(imageBuffer);
+        const image = cloudinary.uploader.upload_stream({
+            resource_type: 'image',
+        }, async (error) => {
+            if (error) {
+                await sock.sendMessage(userId, {
+                    text: 'Terjadi kesalahan saat mengunggah gambar. Silahkan coba lagi.'
+                });
+                console.error(error);
+                return;
+            } else {
+                await sock.sendMessage(userId, {
+                    text: `Terima kasih atas konfirmasi transfer. Bukti transfer anda: ${image.secure_url}`
+                });
+                userState.currentMenu = 'mainMenu';
+                return;
+            }
+        }).end(imageBuffer);
+    } else {
         await sock.sendMessage(userId, {
-            text: `Terima kasih atas konfirmasi transfer. Bukti transfer anda: ${image.secure_url}`
+            text: 'Silahkan kirimkan gambar bukti transfer.'
         });
     }
 };
@@ -217,3 +338,12 @@ const sendMainMenu = async (sock, userId) => {
 
 // Start the socket connection
 startSock();
+
+mongoose
+    .connect(
+        process.env.MONGO_URI
+    ).then(result => {
+        console.log('Connected to MongoDB' + result);
+    }).catch(err => {
+        console.log(err);
+    });
